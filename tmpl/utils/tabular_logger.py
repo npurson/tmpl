2e041e1ee1826d7386d5ce07a81903239a51423f
Haoyi Jiang <natterpurson@qq.com>
2023-04-26 20:27:04 +0800

    update metrics2str in tabular_logger.py

--- tmpl/utils/tabular_logger.py
+++ tmpl/utils/tabular_logger.py
@@ -29,9 +29,9 @@ class ConsoleExperimentWriter(ExperimentWriter):
 
         def metrics2str(metrics: dict) -> str:
             return ', '.join([
-                '{}: {{{}}}'.format(x, metrics2str(y)) if isinstance(y, dict) else
-                '{}: {:.5f}'.format(x, y) if isinstance(y, float) else '{}: {}'.format(x, y)
-                for x, y in metrics.items()
+                f'{x}: {{{metrics2str(y)}}}' if isinstance(y, dict) else
+                (f'{x}: {y:.4f}' if y >= 1e-4 else f'{x}: {y:.5f}')
+                if isinstance(y, float) else f'{x}: {y}' for x, y in metrics.items()
             ])
 
         log_str = metrics2str(metrics)
